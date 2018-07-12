4a4d852044f7ead9594c937abab3c4054b19a010
ikeky <andrew.torgovkin@mail.ru>
2018-07-12 14:15:12 +0900

    five stage

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
 	<title>Пост</title>
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Ubuntu:300,300i,400,400i,500,500i,700,700i&amp;subset=cyrillic" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 <body style="background-color: orange">
